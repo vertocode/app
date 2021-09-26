fd0e0f4b8624255903f7f96cd82fcd5dd67a652e
Everton Vanoni <86416772+vertocode@users.noreply.github.com>
2021-09-25 23:40:44 -0300

    update-commit

---
+++ app/Hello.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <input v-model="title">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Hello",
+    data: function() {
+      return {
+        title: 'Hello VueJs'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
