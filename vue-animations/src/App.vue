a02c4f4122a8a97d2fce043b77ed6ff1076ca1e4
Everton Vanoni <86416772+vertocode@users.noreply.github.com>
2021-09-27 12:03:06 -0300

    Add files via upload

---
+++ vue-animations/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Animações</h1>
+        <p class="lead">Treinando transição/animação de elementos/components no Vue.</p>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <h3 class="font-weight-light">Animações de estado</h3>
+
+      <div class="form-group">
+        <input class="form-control" v-model="numero" step="50">
+      </div>
+
+      <div class="alert alert-info">
+        <h3 class="font-weight-light">
+          <strong>Número: </strong>
+          <span>{{ numeroAnimado }}</span>
+        </h3>
+      </div>      
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+import { TweenLite } from 'gsap/TweenLite'
+
+export default {
+  data() {
+    return {
+      numero: 0,
+      numeroInterpolado: 0
+    }
+  },
+  computed: {
+    numeroAnimado() {
+      return this.numeroInterpolado.toFixed(0)
+    }
+  },
+  watch: {
+    numero(novoNumero, antigoNumero) {
+      TweenLite.to(this.$data, 2, { numeroInterpolado: novoNumero })
+    }
+  }
+}
+
+</script>
+
